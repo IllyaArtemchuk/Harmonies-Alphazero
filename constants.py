2a454b0881d39dbecbc52a6ec5215c48dfe095f1
Illya <ilyalinkis@gmail.com>
2025-04-28 11:56:09 -0700

    change hexes!

--- constants.py
+++ constants.py
@@ -3,16 +3,16 @@ TILE_TYPES = ["water", "plant", "wood", "stone", "building", "field"]
 # Pre-defined hex grid with 5-4-5-4-5 pattern (23 hexes total)
 VALID_HEXES = {
     # r = -2 (Top row, 5 hexes)
-    (-2, -2),
     (-1, -2),
-    (0, -2),
+    (-0, -2),
     (1, -2),
     (2, -2),
+    (3, -2),
     # r = -1 (Second row, 4 hexes)
-    (-2, -1),
     (-1, -1),
     (0, -1),
     (1, -1),
+    (2, -1),
     # r = 0 (Middle row, 5 hexes)
     (-2, 0),
     (-1, 0),
@@ -25,11 +25,11 @@ VALID_HEXES = {
     (0, 1),
     (1, 1),
     # r = 2 (Bottom row, 5 hexes)
+    (-3, 2),
     (-2, 2),
     (-1, 2),
     (0, 2),
     (1, 2),
-    (2, 2),
 }
 
 AXIAL_DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, 1)]
